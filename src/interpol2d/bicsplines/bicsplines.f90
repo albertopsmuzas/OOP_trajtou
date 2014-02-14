46eb0d30a07efff032ecdabdbf1ec02fa255e749
Alberto Muzas <alberto.muzas@uam.es>
2014-02-14 19:30:27 +0100

    Start new 2D interpolation scheme

---
+++ src/interpol2d/bicsplines/bicsplines.f90
@@ -0,0 +1,12 @@
+!##########################################################
+! MODULE: BICUBICSPLINES
+!
+!> @brief 
+!! Provides tools to perform bicubic splines interpolations on
+!! 2D functions
+!##########################################################
+MODULE BICSPLINES_MOD
+USE CSPLINES_MOD
+IMPLICIT NONE
+
+END MODULE BICSPLINES_MOD
