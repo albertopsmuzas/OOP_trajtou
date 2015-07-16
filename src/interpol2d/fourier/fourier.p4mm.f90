5550391b752d26e4dbf05965d6c869a84680fd9b
Alberto Muzas <alberto.muzas@uam.es>
2015-07-16 10:34:09 +0200

    Doh! correction on B2 representation fourier2d p4mm

--- src/interpol2d/fourier/fourier.p4mm.f90
+++ src/interpol2d/fourier/fourier.p4mm.f90
@@ -105,7 +105,7 @@ function termFoup4mm(this,k,parity,irrep,x) result(answer)
    case('B2')
       select case( parity )
       case('+')
-         answer=dsin( g*dfloat(k(1))*x(1) )*dsin( g*dfloat(k(2))*x(2) )-&
+         answer=dsin( g*dfloat(k(1))*x(1) )*dsin( g*dfloat(k(2))*x(2) )+&
                 dsin( g*dfloat(k(2))*x(1) )*dsin( g*dfloat(k(1))*x(2) )
       case default
          write(0,*) 'ERR '//routinename//' parity and irrep selection is not compatible'
@@ -175,7 +175,7 @@ function termfoup4mm_dx(this,k,parity,irrep,x) result(answer)
    case('B1')
       select case( parity )
       case('+')
-         answer=-g*dfloat(k(1))*dsin( g*dfloat(k(1))*x(1) )*dcos( g*dfloat(k(2))*x(2) )+&
+         answer=-g*dfloat(k(1))*dsin( g*dfloat(k(1))*x(1) )*dcos( g*dfloat(k(2))*x(2) )-&
                  g*dfloat(k(2))*dsin( g*dfloat(k(2))*x(1) )*dcos( g*dfloat(k(1))*x(2) )
       case default
          write(0,*) 'ERR '//routinename//' parity and irrep selection is not compatible'
@@ -185,7 +185,7 @@ function termfoup4mm_dx(this,k,parity,irrep,x) result(answer)
    case('B2')
       select case( parity )
       case('+')
-         answer=g*dfloat(k(1))*dcos( g*dfloat(k(1))*x(1) )*dsin( g*dfloat(k(2))*x(2) )-&
+         answer=g*dfloat(k(1))*dcos( g*dfloat(k(1))*x(1) )*dsin( g*dfloat(k(2))*x(2) )+&
                 g*dfloat(k(2))*dcos( g*dfloat(k(2))*x(1) )*dsin( g*dfloat(k(1))*x(2) )
       case default
          write(0,*) 'ERR '//routinename//' parity and irrep selection is not compatible'
@@ -264,7 +264,7 @@ function termfoup4mm_dy(this,k,parity,irrep,x) result(answer)
    case('B2')
       select case( parity )
       case('+')
-         answer=g*dfloat(k(2))*dsin( g*dfloat(k(1))*x(1) )*dcos( g*dfloat(k(2))*x(2) )-&
+         answer=g*dfloat(k(2))*dsin( g*dfloat(k(1))*x(1) )*dcos( g*dfloat(k(2))*x(2) )+&
                 g*dfloat(k(1))*dsin( g*dfloat(k(2))*x(1) )*dcos( g*dfloat(k(1))*x(2) )
       case default
          write(0,*) 'ERR '//routinename//' parity and irrep selection is not compatible'
